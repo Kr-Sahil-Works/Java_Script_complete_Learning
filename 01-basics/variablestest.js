1bdeba9dea01e79fc66ab4e4a193d10419887cd1
Kr7-Sahil <167867305+Kr7-Sahil@users.noreply.github.com>
2024-04-24 13:56:02 +0000

    ok done

---
+++ 01-basics/variablestest.js
@@ -0,0 +1,28 @@
+const account_Id = 13443223
+let account_email = "samtheuser24@gmail.com"
+let password = "12345"
+let  accountcity = "bangalore"
+let accountState ;  // semi-colon in java script is optional you can use it or skip it 
+
+
+// var password = "12345"      // just donot use var reason mentioned below 
+// accountcity = "bangalore"   // donot use like this but can be used like this 
+
+
+
+
+// account_Id = 23  // ctrl + / shortcut for commenting   // not allowed 
+console.log(account_Id);
+
+account_email = "yes@ok.com"
+password = "2334"
+accountcity = "jaipur"
+
+
+/*
+Donot use var for variable decleration 
+because of block Scope and functional scope in JavaScript
+var is global scope and can be accessed from anywhere or everywhere easily 
+*/ 
+
+console.table([account_Id, account_email , password , accountcity ,accountState ])
