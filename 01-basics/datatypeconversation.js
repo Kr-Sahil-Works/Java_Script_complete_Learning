7c8e299de0e81bcf99aafb5ccec2d57d2ca0067b
Kr7-Sahil <167867305+Kr7-Sahil@users.noreply.github.com>
2024-04-29 08:51:32 +0000

    ok

--- 01-basics/datatypeconversation.js
+++ 01-basics/datatypeconversation.js
@@ -1,7 +1,34 @@
-let score = 34
-//  score ="33"
+let score = "34"
+let score2 = 78
+let imaginescore = "33abc"
+let imaginescore2 = 3219023
 
-console.log(typeof score)
-console.log(typeof (score) )
+console.log("original score value is "+typeof score)
+console.log("original score2 value is "+typeof score2)
+// console.log(typeof (score) )
 
-console.table([typeof score,typeof (score)])
+// console.table([typeof score,typeof (score)])
+
+let valueInNumber = Number(score)
+let valueInString = String(score2)
+
+// console.table([typeof valueInNumber])
+// console.table([typeof valueInString])
+
+let valueInNum = Number (imaginescore)
+let valueInStr = String (imaginescore2)
+
+console.log(typeof valueInNum)
+console.log(typeof valueInStr)
+
+console.log(valueInNum)
+console.log(valueInStr)
+
+let x = imaginescore*2
+let y = imaginescore + "ok"
+let a = imaginescore2 * 2
+let b = imaginescore2 + you
+console.log(x)
+console.log(y)
+
+console
