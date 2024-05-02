5d9a8e20447512d353f2de8050d80b90e2cd01e3
Kr7-Sahil <167867305+Kr7-Sahil@users.noreply.github.com>
2024-05-02 13:36:35 +0000

    ok

--- 01-basics/05-string.js
+++ 01-basics/05-string.js
@@ -1,11 +1,11 @@
 const name = "sam"
 let score = 98
 let extrapoint = 2
-// console.log(name + " "+ score + " "+ "value");
+// console.log(name + " "+ score + " "+ "value");  //***** Donot use this type of syntax ******
 
 
-console.log(`Hello my name is ${name} and my total score is ${score + extrapoint}`);
-
+// console.log(`Hello my name is ${name} and my total score is ${score + extrapoint}`);
+//************** very very important to use this type of syntax in morden programming *************** 
 const nameobj = new String('NobitaBoyok')
 
 // console.log(nameobj [0]);
@@ -17,8 +17,42 @@ const nameobj = new String('NobitaBoyok')
 const newString = nameobj.substring(0,5)  // ending value will not come 
 // Syntax => stringname.substring (starting_position , ending_position )
 
-console.log(newString);
+// console.log(newString);
 
 const otherString = nameobj.slice(-4,4)  // Here, negative or backward slicing can happen 
 //  which is not present in the substring 
-console.log(otherString);
+// console.log(otherString);
+
+const newStringone = "        Nobitatheuser      "
+
+// console.log(newStringone);
+// console.log(newStringone.trim());
+
+const url = "https://flickflights%20netlify.app"
+
+// console.log(url.replace('%20','.'));
+
+// console.log(url.includes('flight'));
+// console.log(url.includes('aeroplane'));
+
+// *******************************************************************
+
+const demotext = new String('Testme-pleaseIamhere')
+
+// console.log(demotext.anchor);  // html anchor related
+console.log(demotext.charAt(4)); // position
+// console.log(demotext.big()); // html big tag related
+// console.log(demotext.blink()); // html blink tag related
+console.log(demotext.charCodeAt(4));
+console.log(demotext.codePointAt(4));
+console.log(demotext.concat(' added-text'));
+console.log(demotext.endsWith('here'));  // used to check whether it ends with certain word/letter or not
+// console.log(demotext.fixed());  // I DONOT KNOW
+// console.log(demotext.fontsize('50px'));  // HTML
+// console.log(demotext.fontcolor('black')); // HTML
+// console.log(demotext.italics()); //HTML
+
+const searchword = 'me'
+console.log(`index of the last ${searchword} is ${demotext.lastIndexOf(searchword)}`);
+// ******************** Very Important***************************
+
