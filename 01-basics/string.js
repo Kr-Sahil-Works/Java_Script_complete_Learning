cc673b35fd024ed80c0f28948ef0c40a57e89969
Kr7-Sahil <167867305+Kr7-Sahil@users.noreply.github.com>
2024-05-02 11:09:32 +0000

    half-done

---
+++ 01-basics/string.js
@@ -0,0 +1,16 @@
+const name = "sam"
+let score = 98
+let extrapoint = 2
+console.log(name + " "+ score + " "+ "value");
+
+
+console.log(`Hello my name is ${name} and my total score is ${score + extrapoint}`);
+
+const nameobj = new String("Nobita")
+
+console.log(nameobj [0]);
+console.log(nameobj.length);
+console.log(nameobj.toLowerCase());
+console.log(nameobj.charAt(4));
+console.log(nameobj.indexOf('a'));
+
