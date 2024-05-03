83737b2d4f3ed7334d8b2d8131246e8d4f0200e1
Kr7-Sahil <167867305+Kr7-Sahil@users.noreply.github.com>
2024-05-03 02:21:39 +0000

    done

--- 01-basics/05-string.js
+++ 01-basics/05-string.js
@@ -39,30 +39,43 @@ const url = "https://flickflights%20netlify.app"
 
 const demotext = new String('Testme-pleaseIamhere')
 
-// console.log(demotext.anchor);  // html anchor related
+// console.log(demotext.anchor);  // not used now
+// console.log(demotext.big()); // not used now
+// console.log(demotext.blink()); // not used now
+// console.log(demotext.blod()); // not used now
+
+// console.log(demotext.fixed());  // not used now
+// console.log(demotext.fontsize('50px'));  // not used now
+// console.log(demotext.fontcolor('black')); // not used now
+// console.log(demotext.italics()); //not used now
+
+// ********************* no longer recommended at all  *********************
+
+// console.log(demotext.link()); //not used now
+// console.log(demotext.small()); //not used now
+// console.log(demotext.sub()); //not used now
+// console.log(demotext.substring()); //not used now
+// console.log(demotext.sup()); //not used now
+
+// console.log(demotext.localeCompare);
+// console.log(demotext.match());                // I Donot know the use
+// console.log(demotext.normalize());
+
+
 console.log(demotext.charAt(4)); // position
-// console.log(demotext.big()); // html big tag related
-// console.log(demotext.blink()); // html blink tag related
 console.log(demotext.charCodeAt(4));
 console.log(demotext.codePointAt(4));
 console.log(demotext.concat(' added-text'));
-console.log(demotext.endsWith('here'));  // used to check whether it ends with certain word/letter or not
-// console.log(demotext.fixed());  // I DONOT KNOW
-// console.log(demotext.fontsize('50px'));  // HTML
-// console.log(demotext.fontcolor('black')); // HTML
-// console.log(demotext.italics()); //HTML
 
-const searchword = 'me'
-console.log(`index of the last ${searchword} is ${demotext.lastIndexOf(searchword)}`);
-// ******************** Very Important***************************
+console.log(demotext.startsWith('Test'))  // used to check whether it [starts] with certain word/letter or not
+console.log(demotext.endsWith('here'));  // used to check whether it [ends] with certain word/letter or not
 
 console.log(demotext.length);
-// console.log(demotext.link(url:'text'));  // no longer recommended
 
-// console.log(demotext.localeCompare);
-// console.log(demotext.match());
-// console.log(demotext.normalize());
+const searchword = 'me'
 
+console.log(`index of the last ${searchword} is ${demotext.lastIndexOf(searchword)}`);
+// ******************** Very Important***************************
 
 const str19 = 'Breaded Mushrooms';
 
@@ -75,3 +88,16 @@ console.log(str29.padEnd(5, '-'));
 // Expected output: "200  "
 console.log(str29.padStart(10, '_'));
 
+console.log(demotext.repeat(2)); // very useful
+
+console.log(demotext.replaceAll('e','-')) // very useful  // here all the characther are been replaced at once
+console.log(demotext.replace('e','-'))  //only replace the first characther
+
+
+
+console.log(demotext.search('me'))
+
+console.log(demotext.toLocaleLowerCase()) // returns whole sentence in lower
+
+console.log(demotext.toLocaleUpperCase()) // returns whole sentence in upper
+console.log(demotext.toString());
