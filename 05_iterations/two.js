a702ab4e2f3f671996a5c50965ca98ec62242505
Sahil K <167867305+Kr-Sahil-Works@users.noreply.github.com>
2024-05-11 11:16:49 +0000

    Done ok

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let index = 1
+
+while (index <= 20 ) {
+    // console.log(`value of index : ${index}`);
+    index = index +2
+}
+
+let myarr = [1,2,34,5,6,77,8]
+let len = myarr.length
+let i = 0
+// while (i < len) {
+    // console.log(myarr[i]);
+//      i = i +1
+//     }
+
+let score = 11
+
+do {
+    
+    console.log(`socre is : ${score}`);
+    score ++
+} while (score <=10);
