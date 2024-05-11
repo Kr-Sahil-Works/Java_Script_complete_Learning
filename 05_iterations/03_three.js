c3f3061406d808b1fe46ae392e81f2d241b54782
Sahil K <167867305+Kr-Sahil-Works@users.noreply.github.com>
2024-05-11 13:51:15 +0000

    Done ok

---
+++ 05_iterations/03_three.js
@@ -0,0 +1,55 @@
+//  for of 
+
+// ["","",""]
+
+//  [ [],[],[] ]
+
+// [ {},{},{} ]
+
+const arr = [1,2,3,4,5]
+
+// for (const iterator of object) {   iterator = variable used  & object refer to which object we are refering for the loop it can be a array also strings also and object also
+    
+// }
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const welcome = ["hi","hello","bye-bye"]
+
+for (const str of welcome) {
+    // console.log(str);
+}
+
+const greeting = "Hello Nobita"
+for (const greet of greeting) {
+    // console.log(`Each char is : ${greet}`);
+}
+
+
+//  Map
+
+const map = new Map()  // Orders & Umique values
+
+map.set("IN","India")
+map.set("IN","India")  // No duplicates
+map.set("US","America")
+map.set("UK","England")
+map.set("AU","Austrilia")
+
+// console.log(map);
+
+for (const [code , country] of map) {
+    // console.log(code,"  :- ",country);
+}
+
+const myobj = {
+    game1 :"NFS",
+    game2 : "Temple Run",
+}
+
+// for (const [gno,gname] of myobj) {
+//     console.log(gname);
+// }
+
