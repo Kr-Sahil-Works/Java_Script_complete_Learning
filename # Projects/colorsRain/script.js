26468e6d4c04a0443179f5b021099358e449c1e8
Sahil K <167867305+Kr-Sahil-Works@users.noreply.github.com>
2024-06-09 03:29:48 +0000

    ok

---
+++ # Projects/colorsRain/script.js
@@ -0,0 +1,12 @@
+//  Generate a random color
+
+const randomColor = function () {
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        const element = array[i];
+        
+    }
+}
+
+(Math.random(hex)*16)
