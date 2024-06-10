ca07736de4b808023012b0c428bd9502e5687891
Sahil K <167867305+Kr-Sahil-Works@users.noreply.github.com>
2024-06-10 08:25:29 +0000

    ok

--- # Projects/colorsRain/script.js
+++ # Projects/colorsRain/script.js
@@ -9,4 +9,26 @@ const randomColor = function () {
     return color;
 }
 
+let interval_id;
+
 console.log(randomColor())
+
+function startcolorchanging (){
+     
+    if (!interval_id){
+     interval_id = setInterval(colorrain,500)
+    }
+     function colorrain (){
+        document.body.style.backgroundColor= randomColor();
+    }
+     
+}
+
+function stopcolorchanging() {
+    clearInterval(interval_id)
+    interval_id = null ; 
+}
+
+document.querySelector('#start').addEventListener('click',startcolorchanging)
+
+document.querySelector('#stop').addEventListener('click', stopcolorchanging)
